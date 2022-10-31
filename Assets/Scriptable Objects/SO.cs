e343855b383b97f9a324168c804035c66ab41b8f
MelnikVladyslav <programmergamingvlad@gmail.com>
2022-10-31 17:36:29 +0200

    Add SO Regions, Divisions, Tech and Country

--- Assets/Scriptable Objects/SO.cs
+++ Assets/Scriptable Objects/SO.cs
@@ -22,6 +22,24 @@ public enum TypePolk
     Btr
 }
 
+public enum TypeCountry
+{
+    Neutrall,
+    Player,
+    Enemy,
+    Alliens
+}
+
+public enum Idelogies
+{
+    Neutrall,
+    Communism,
+    Fascism,
+    Nationalism,
+    Democraty,
+    Monarchy
+}
+
 
 [CreateAssetMenu(fileName = "Build", menuName = "Create objects/Build")]
 public class Build : ScriptableObject
@@ -40,5 +58,56 @@ public class Polk : ScriptableObject {
     public string Price;
     public string Hit;
     public string Step;
+
+}
+
+[CreateAssetMenu(fileName = "Division", menuName = "Create objects/Division")]
+public class Division : ScriptableObject
+{
+
+    public string Name;
+    public List<Polk> polks;
+
+}
+
+[CreateAssetMenu(fileName = "Region", menuName = "Create objects/Region")]
+public class Region : ScriptableObject
+{
+
+    public string Name;
+    public GameObject region;
+    public GameObject town;
+    public List<Division> divisions;
+    public List<Build> builds;
+    public int Population;
+
+}
+
+[CreateAssetMenu(fileName = "Technology", menuName = "Create objects/Technology")]
+public class Technology : ScriptableObject
+{
+
+    public string Name;
+    public int Time;
+    public List<Technology> TechsNeed;
+    public List<Build> builds;
+    public List<Polk> polks; 
+
+}
+
+[CreateAssetMenu(fileName = "Country", menuName = "Create objects/Country")]
+public class Country : ScriptableObject
+{
+
+    public string Name;
+    public TypeCountry Types;
+    public int PopulationCount;
+    public string NameAlliens;
+    public List<Region> regions;
+    public List<Technology> techs;
+    public Idelogies idelogy;
+    public int Stabilnisty;
+    public int ProcentViyskovoZob;
+    public int KilkistyRecruit;
 
 }
