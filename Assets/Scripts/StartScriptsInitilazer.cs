11c15af43be0e7549189bf5b4d5233fa0917aef1
MelnikVladyslav <programmergamingvlad@gmail.com>
2022-12-11 15:19:26 +0200

    Update

--- Assets/Scripts/StartScriptsInitilazer.cs
+++ Assets/Scripts/StartScriptsInitilazer.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -16,7 +17,7 @@ public class StartScriptsInitilazer : MonoBehaviour
 
     public void Start()
     {
-       
+        CreateLists();
     }
     public void Update()
     {
