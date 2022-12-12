8b207b26caf727c2d6ad522e70166ee6d8c181ac
MelnikVladyslav <programmergamingvlad@gmail.com>
2022-12-12 14:51:15 +0200

    Update game

---
+++ Assets/Scripts/Navigatiion/NavigationUp.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Navigatiion
+{
+    public class NavigationUp : MonoBehaviour
+    {
+        public EnterNation enterNation;
+        public RawImage Flag;
+        public Text TextStab;
+        public Text TextRecriut;
+        public Text TextMoney;
+        public Text TextBuilsRes;
+        public Text TextStail;
+        public Text TextTopluvo;
+        public Text TextKauchuk;
+
+        // Use this for initialization
+        void Start()
+        {
+            Flag.texture = enterNation.countryPlayer.Flag;
+            TextStab.text = enterNation.countryPlayer.Stabilnisty.ToString();
+            TextRecriut.text = enterNation.countryPlayer.KilkistyRecruit.ToString();
+            TextMoney.text = enterNation.countryPlayer.Money.ToString();
+            TextBuilsRes.text = enterNation.countryPlayer.BuildResourse.ToString();
+            TextStail.text = enterNation.countryPlayer.Stail.ToString();
+            TextTopluvo.text = enterNation.countryPlayer.Topluvo.ToString();
+            TextKauchuk.text = enterNation.countryPlayer.Kauchuk.ToString();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
