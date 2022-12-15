b56b8add78ff18fc457bb4fbe9e2d37fc6fa3619
MelnikVladyslav <programmergamingvlad@gmail.com>
2022-12-15 18:39:16 +0200

    Add RichenyaMechanic

---
+++ Assets/Scripts/Navigatiion/RichenyaMechanics.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Navigatiion
+{
+    public class RichenyaMechanics : MonoBehaviour
+    {
+        public EnterNation enterNation;
+        public int ballDem = 0;
+        public int ballNeu = 0;
+        public int ballcom = 0;
+        public int ballFac = 0;
+        public int ballNac = 0;
+        public int ballMon = 0;
+        public Text ProcPP;
+        public Text ProcOP;
+
+        public void PidtDem()
+        {
+            ballDem++;
+        }
+        public void PidtNeu()
+        {
+            ballNeu++;
+        }
+        public void Pidtcom()
+        {
+            ballcom++;
+        }
+        public void PidtFac()
+        {
+            ballFac++;
+        }
+        public void PidtNac()
+        {
+            ballNac++;
+        }
+        public void PidtMon()
+        {
+            ballMon++;
+        }
+    }
+}
